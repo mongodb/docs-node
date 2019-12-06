1255fd1459cd695a6b0402c95cd2cf91f93d93ee
nathan-contino-mongo <nathan.contino@mongodb.com>
2019-12-06 11:18:30 -0500

    nathan feedback; query links

--- source/code-snippets/usage-examples/find.js
+++ source/code-snippets/usage-examples/find.js
@@ -1,38 +1,37 @@
 
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb')
 
 
 const client = new MongoClient(
    'mongodb+srv://admin:admin@mflix-2sp0m.mongodb.net/test?retryWrites=true&w=majority',
-);
+)
 
 async function run() {
    try {
-      await client.connect();
+      await client.connect()
 
-      const database = client.db('sample_mflix');
-      const collection = database.collection('movies');
+      const database = client.db('sample_mflix')
+      const collection = database.collection('movies')
 
       // query for movies that have a runtime less than 15 minutes
-      const query = { title: "Black Sheep" };
+      const query = { runtime: { $lt : 15 } }
+      // sort returned documents in ascending order by title (A->Z)
+      const sort = { title: 1 }
+      // Include only the `title` and `fullplot` fields in each returned document
+      const projection = { title : 1, _id: 0, fullplot: 1}
 
-      // initialize an empty options object
-      const options = {
-         // sort returned documents in ascending order by title (A->Z)
-         sort: { title: 1 },
-         // Include only the `title` and `imdb` fields in each returned document
-         projection: { title : 1, _id: 0, imdb: 1, fullplot: 1}
-      }
-
-      // pass our query and constructed options
-      const cursor = collection.find(query, options);
+      // pass our query, sort, and projection
+      const cursor = collection
+         .find(query)
+         .sort(sort)
+         .project(projection)
 
       // iterate with await so the session doesn't end while printing
       for await (const movie of cursor) {
-         console.dir(movie);
+         console.dir(movie)
       }
    } finally {
-      await client.close();
+      await client.close()
    }
 }
-run().catch(console.dir);
+run().catch(console.dir)
