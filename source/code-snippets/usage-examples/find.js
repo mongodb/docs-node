3db99f266e1dde8caf2727406b8bd13a402971b3
nathan-contino-mongo <nathan.contino@mongodb.com>
2019-12-06 13:16:20 -0500

    nathan side-by-side feedback

--- source/code-snippets/usage-examples/find.js
+++ source/code-snippets/usage-examples/find.js
@@ -1,42 +1,37 @@
-
-const { MongoClient } = require('mongodb')
-
+const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(
-   'mongodb+srv://admin:admin@mflix-2sp0m.mongodb.net/test?retryWrites=true&w=majority',
-)
+  "mongodb+srv://admin:admin@mflix-2sp0m.mongodb.net/test?retryWrites=true&w=majority",
+);
 
 async function run() {
-   try {
-      await client.connect()
+  try {
+    await client.connect();
 
-      const database = client.db('sample_mflix')
-      const collection = database.collection('movies')
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
 
-      // query for movies that have a runtime less than 15 minutes
-      const query = { runtime: { $lt : 15 } }
-      // sort returned documents in ascending order by title (A->Z)
-      const sort = { title: 1 }
-      // Include only the `title` and `fullplot` fields in each returned document
-      const projection = { title : 1, _id: 0, fullplot: 1}
+    // query for movies that have a runtime less than 15 minutes
+    const query = { runtime: { $lt: 15 } };
+    // sort returned documents in ascending order by title (A->Z)
+    const sort = { title: 1 };
+    // Include only the `title` and `fullplot` fields in each returned document
+    const projection = { title: 1, _id: 0, fullplot: 1 };
 
-      // pass our query, sort, and projection
-      const cursor = collection
-         .find(query)
-         .sort(sort)
-         .project(projection)
+    // pass our query, sort, and projection
+    const cursor = collection
+      .find(query)
+      .sort(sort)
+      .project(projection);
 
-      // print a message if no documents were found
-      if (await cursor.count() == 0) {
-         console.log("No documents found")
-      }
+    // print a message if no documents were found
+    if (!(await cursor.hasNext())) {
+      console.log("No documents found");
+    }
 
-      // iterate with await so the session doesn't end while printing
-      for await (const movie of cursor) {
-         console.dir(movie)
-      }
-   } finally {
-      await client.close()
-   }
+    await cursor.forEach(console.dir);
+  } finally {
+    await client.close();
+  }
 }
-run().catch(console.dir)
+run().catch(console.dir);
