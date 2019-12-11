852815ca1fe864db44a478e9ac4a4b0b70b78697
nathan-contino-mongo <nathan.contino@mongodb.com>
2019-12-11 12:11:12 -0500

    documenting deleteOne instead of findOneAndDelete

--- source/code-snippets/usage-examples/deleteOne.js
+++ source/code-snippets/usage-examples/deleteOne.js
@@ -13,23 +13,22 @@ async function run() {
     const database = client.db("sample_mflix");
     const collection = database.collection("movies");
 
-    // Query for a movie that has the title of type string
+    // Query for a movie that has a title of type string
     const query = { title: { $type: "string" } };
 
     const options = {
-      // sort matched documents in ascending order by rating
-      sort: { rating: 1 },
-      // Include only the `title` and `fullplot` fields in the returned document
-      projection: { _id: 0, title: 1, fullplot: 1 },
+      // timeout after 30 seconds with no response
+      wtimeout: 30000,
+      // confirm write only after majority of replicas have acknowledged
+      w: "majority",
     };
 
-    collection.findOneAndDelete(query, options, function(error, result) {
+    collection.deleteOne(query, options, function(error, result) {
       if (error) {
         console.log("Error: " + error.errmsg);
       } else {
-        if (result.lastErrorObject.n == 1) {
-          console.log("Deleted document:");
-          console.dir(result.value);
+        if (result.deletedCount == 1) {
+          console.dir("Successfully deleted one document.");
         } else {
           console.log("No documents matched the query.");
         }
