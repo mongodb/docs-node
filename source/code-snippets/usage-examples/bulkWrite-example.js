bf0ca7f99bc6d48f3e4789f2a7393922b7718b09
Allison Reinheimer Moore <allison.moore@10gen.com>
2019-12-12 15:38:52 -0500

    DOCSP-7602: standardize code & prose with other examples

--- source/code-snippets/usage-examples/bulkWrite-example.js
+++ source/code-snippets/usage-examples/bulkWrite-example.js
@@ -1,49 +1,59 @@
+const { MongoClient } = require("mongodb");
+const fs = require("fs");
 
-const MongoClient = require("mongodb").MongoClient
-const fs = require("fs")
+// Replace the following with your MongoDB deployment's connection
+// string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
 
-;(async() => {
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+async function run() {
   try {
-    // Replace the following with your MongoDB deployment's connection 
-    // string.
-    const host = 'mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority'
-    const client = await MongoClient.connect(
-      host,
-      { useNewUrlParser: true,
-        useUnifiedTopology: true 
-      },
-    )
-    const mflix = client.db("sample_mflix")
-    
-    // Replace with the path to your copy of the users.json file
-    var users = JSON.parse(fs.readFileSync("<path/to/users.json/file"))
+    await client.connect();
 
-    var usersToInsert = []
+    const mflix = client.db("sample_mflix");
+
+    // Replace with the path to your copy of the users.json file
+    var users = JSON.parse(fs.readFileSync("<path/to/users.json/file"));
+
+    var usersToInsert = [];
 
     // Loop through each user in the users array and format the data from
     // the JSON file to be a series of insertOne operations. Append
     // each appropriately formatted object to the usersToInsert array.
     users.forEach(user => {
-      usersToInsert.push({"insertOne": {"document": user}})
-    })
+      usersToInsert.push({ insertOne: { document: user } });
+    });
 
-    // Perform the bulk write operation and print out the resulting
-    // BulkWriteResult object. This is a routine data import
+    // Perform the bulk write operation and print out the number
+    // of documents inserted and/or any error messages.
+    // This is a routine data import
     // that does not rely on the ordering of the operations to ensure
     // data consistency, so you can set the ordered option to false
     // to improve write performance.
-    var result = await mflix.collection("users").bulkWrite
-    (usersToInsert, { ordered: false })
-    console.log(result)
+    mflix
+      .collection("users")
+      .bulkWrite(usersToInsert, { ordered: false }, function(error, result) {
+        // The bulk write operation may successfully insert documents
+        // and also return an error, so both cases must be handled.
+        if (result.nInserted > 0) {
+          console.log("Number of documents inserted: " + result.nInserted);
+        } else {
+          console.log(
+            "No documents were inserted during the bulk write operation"
+          );
+        }
 
-    // Close the connection to the database and exit the process.
-    client.close()
-    process.exit(0)
-  } catch (e) {
-
-    // Report any errors that come up during the operation and exit the
-    // process.
-    console.error("\x1b[32m", `Error during bulkWrite, ${e}`)
-    process.exit(1)
+        if (error) {
+          console.log("Error: " + error);
+        }
+      });
+  } finally {
+    await client.close();
   }
-})()
+}
+run().catch(console.dir);
