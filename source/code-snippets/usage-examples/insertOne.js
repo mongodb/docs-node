778a65698679650a3c6436c25dbf086668886387
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-12-16 09:39:04 -0500

    (DOCSP-7600): insertOne init

---
+++ source/code-snippets/usage-examples/insertOne.js
@@ -0,0 +1,29 @@
+// ignored first line
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
+    // create a document object
+    const doc = { name: "Red", town: "kanto" };
+    // specify an additional options object
+    const options = {};
+    options.bypassDocumentValidation = true; // bypass document validation
+    const result = await collection.insertOne(doc);
+
+    console.log(
+      `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
