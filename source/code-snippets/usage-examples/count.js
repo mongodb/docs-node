776d00acd1e8433de4acacf5d40de0acacd1a798
Allison Reinheimer Moore <allison.moore@10gen.com>
2019-12-18 11:00:50 -0500

    DOCSP-7609: style fixes + use Promise not callback

--- source/code-snippets/usage-examples/count.js
+++ source/code-snippets/usage-examples/count.js
@@ -20,15 +20,10 @@ async function run() {
 
     // Estimate the total number of documents in the collection
     // and print out the count.
-    collection.estimatedDocumentCount({}, function(error, result) {
-      if (error) {
-        console.log("Error: " + error);
-      } else {
-        console.log(
-          "Estimated number of documents in the movies collection: " + result,
-        );
-      }
-    });
+    var estimate = await collection.estimatedDocumentCount();
+    console.log(
+      "Estimated number of documents in the movies collection: " + estimate,
+    );
 
     // Query for movies from Canada.
     const query = { countries: "Canada" };
@@ -36,13 +31,8 @@ async function run() {
     // Find the number of documents that match the specified
     // query, (i.e. with "Canada" as a value in the "countries" field)
     // and print out the count.
-    collection.countDocuments(query, {}, function(error, result) {
-      if (error) {
-        console.log("Error: " + error);
-      } else {
-        console.log("Number of movies from Canada: " + result);
-      }
-    });
+    var countCanada = await collection.countDocuments(query);
+    console.log("Number of movies from Canada: " + countCanada);
   } finally {
     await client.close();
   }
