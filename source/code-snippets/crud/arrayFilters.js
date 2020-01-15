ffce4824c8b00974562f3bbe34cd6d6c6df9aabe
nathan-contino-mongo <nathan.contino@mongodb.com>
2020-01-15 12:40:49 -0500

    modify arrays

--- source/code-snippets/crud/arrayFilters.js
+++ source/code-snippets/crud/arrayFilters.js
@@ -17,18 +17,59 @@ async function run() {
     await client.connect();
 
     const database = client.db("test");
-    const collection = database.collection("hotdog");
+    const collection = database.collection("pizza");
 
     console.log(JSON.stringify(await (await collection.find()).toArray()));
 
     // start arrayFilters example
-    const result = await collection.updateMany(
-      { "finalists.name": "Steve Jobs" },
-      { $set: { "finalists.$[exampleVariableName].name": "Steve Lobster" } },
-      { arrayFilters: [{ "exampleVariableName.name": "Steve Jobs" }] },
-    );
+    const query = { name: "Steve Lobsters" };
+    const updateDocument = {
+      $push: { "items.$[item].toppings": "salami" },
+    };
+    const options = {
+      arrayFilters: [
+        {
+          "item.type": "pizza",
+          "item.toppings": "pepperoni",
+        },
+      ],
+    };
+    const result = await collection.updateOne(query, updateDocument, options);
     // end arrayFilters example
     console.log(result.modifiedCount);
+
+    collection.insertOne({
+      name: "Steve Lobsters",
+      address: "731 Yexington Avenue",
+      items: [
+        {
+          type: "pizza",
+          size: "large",
+          toppings: ["pepperoni"],
+        },
+        {
+          type: "pizza",
+          size: "medium",
+          toppings: ["mushrooms", "sausage", "green peppers"],
+          comment: "Extra green peppers please!",
+        },
+        {
+          type: "pizza",
+          size: "large",
+          toppings: ["pineapple, ham"],
+          comment: "red pepper flakes on top",
+        },
+        {
+          type: "calzone",
+          fillings: ["canadian bacon", "sausage", "onion"],
+        },
+        {
+          type: "beverage",
+          name: "Diet Pepsi",
+          size: "16oz",
+        },
+      ],
+    });
 
     console.log(JSON.stringify(await (await collection.find()).toArray()));
   } finally {
