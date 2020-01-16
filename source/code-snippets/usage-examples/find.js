92d0db1ddedd53b9cf4daef89244f9fd822d7ba1
nathan-contino-mongo <nathan.contino@mongodb.com>
2020-01-16 10:57:42 -0500

    chris comment

--- source/code-snippets/usage-examples/find.js
+++ source/code-snippets/usage-examples/find.js
@@ -32,7 +32,7 @@ async function run() {
       .project(projection);
 
     // print a message if no documents were found
-    if ((await cursor.count()) == 0) {
+    if ((await cursor.count()) === 0) {
       console.log("No documents found!");
     }
 
