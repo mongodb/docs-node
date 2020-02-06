387992630ac202e81cb497b84b2f3374b47c53fe
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-02-06 13:29:05 -0500

    added await to client connection

--- source/code-snippets/faq/econnresetWithClientConnect-example.js
+++ source/code-snippets/faq/econnresetWithClientConnect-example.js
@@ -2,6 +2,6 @@ const uri = "mongodb://localhost:27017/test?maxPoolSize=5000";
 // create a new MongoClient
 const client = new MongoClient(uri);
 
-client.connect(err => {
+await client.connect(err => {
   // connection
 });
