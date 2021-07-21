c3c62cfcb55fb374d988a7793297404108e92d50
rustagir <rea.rustagi@mongodb.com>
2021-07-21 11:35:25 -0400

    small fixes to grammar and order

--- source/code-snippets/authentication/aws.js
+++ source/code-snippets/authentication/aws.js
@@ -4,9 +4,9 @@ const { MongoClient } = require("mongodb");
 const accessKeyId = encodeURIComponent("<AWS_ACCESS_KEY_ID>");
 const secretAccessKey = encodeURIComponent("<AWS_SECRET_ACCESS_KEY>");
 const clusterUrl = "<MongoDB cluster url>";
+
 const authMechanism = "MONGODB-AWS";
 
-// Replace the following with your MongoDB deployment's connection string.
 let uri =
   `mongodb+srv://${accessKeyId}:${secretAccessKey}@${clusterUrl}/?authMechanism=${authMechanism}`;
 
@@ -17,7 +17,6 @@ let uri =
 // Create a new MongoClient.
 const client = new MongoClient(uri);
 
-// Function to connect to the server.
 async function run() {
   try {
     // Connect the client to the server.
@@ -25,7 +24,7 @@ async function run() {
 
     // Establish and verify connection.
     await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
+    console.log("Connected successfully to server.");
   } finally {
     // Ensure that the client will close when you finish/error.
     await client.close();
