cba2ed062bd2428000c99d8578061baa0185597e
Aleksander Binion <alek.binion@mongodb.com>
2021-08-05 13:41:18 -0400

    add typescript example

---
+++ source/code-snippets/usage-examples/findOne.ts
@@ -0,0 +1,39 @@
+import { FindOptions, MongoClient } from "mongodb";
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
+
+const client = new MongoClient(uri);
+
+type imdb = {
+  rating:number,
+  votes:number,
+  id:number
+}
+
+interface MovieSchema extends Document {
+  title:string,
+  imdb:imdb
+}
+
+async function run(): Promise<void> {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const movies = database.collection<MovieSchema>("movies");
+    const query = { title: "The Room" } as Document;
+
+    const options = {
+      sort: { rating: -1 },
+      projection: { _id: 0, title: 1, imdb: 1 },
+    } as FindOptions;
+
+    const movie = await movies.findOne(query, options);
+    console.log(movie);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
