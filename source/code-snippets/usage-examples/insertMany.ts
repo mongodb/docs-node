7beab7064cd2de3e4817ebb7c6ea636079f38318
Aleksander Binion <alek.binion@mongodb.com>
2021-08-06 14:08:47 -0400

    add comment

--- source/code-snippets/usage-examples/insertMany.ts
+++ source/code-snippets/usage-examples/insertMany.ts
@@ -16,13 +16,15 @@ async function run() {
     await client.connect();
 
     const database = client.db("insert_db");
+    // Specifying a schema is optional, but it enables type hints on
+    // finds and inserts
     const foods = database.collection<Food>("foods");
 
     // create an array of documents to insert
     const docs = [
       { name: "cake", healthy: false },
       { name: "lettuce", healthy: true },
-      { name: "donut", healthy: false }
+      { name: "donut", healthy: false },
     ];
 
     // this option prevents additional documents from being inserted if one fails
