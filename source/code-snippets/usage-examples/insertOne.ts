067271dbf1673fc5563dec698357da1fb90eb078
Aleksander Binion <alek.binion@mongodb.com>
2021-08-06 09:17:19 -0400

    add typescript example

---
+++ source/code-snippets/usage-examples/insertOne.ts
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
+
+const client = new MongoClient(uri);
+
+interface Town {
+  name: string;
+  region: string;
+}
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    // Specifying a Schema is optional, but it enables type hints on
+    // finds and 'inserts
+    const movies = database.collection<Town>("movies");
+    // create a document to be inserted
+    const result = await movies.insertOne({ name: "Red", region: "Kanto" });
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
