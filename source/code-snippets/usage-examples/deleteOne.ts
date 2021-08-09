4611fbcffc5bb6ff5cbdcb16c069aed06d7f87ac
Aleksander Binion <alek.binion@mongodb.com>
2021-08-09 12:19:30 -0400

    add ts example and output

---
+++ source/code-snippets/usage-examples/deleteOne.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+
+    const result = await movies.deleteOne({ title: { $type: "string" } });
+    if (result.deletedCount === 1) {
+      console.dir("Successfully deleted one document.");
+    } else {
+      console.log("No documents matched the query. Deleted 0 documents.");
+    }
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
