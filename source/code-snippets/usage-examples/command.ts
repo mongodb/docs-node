de440319147ff93577533a7a715174b54490454c
Aleksander Binion <alek.binion@mongodb.com>
2021-08-09 10:02:26 -0400

    add ts example

---
+++ source/code-snippets/usage-examples/command.ts
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("sample_mflix");
+    // find the storage statistics for the "sample_mflix" database using the 'dbStats' command
+    const result = await db.command({
+      dbStats: 1,
+    });
+    console.log(result);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
