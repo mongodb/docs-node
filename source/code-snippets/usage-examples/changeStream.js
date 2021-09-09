9d85fdeb8bd7d45786bae19cf8e42da0d9b966f6
Kailie Yuan <kailie.yuan@10gen.com>
2021-09-09 16:27:45 -0400

    cleaned up previous code

--- source/code-snippets/usage-examples/changeStream.js
+++ source/code-snippets/usage-examples/changeStream.js
@@ -7,23 +7,29 @@ const client = new MongoClient(uri);
 
 const simulateAsyncPause = () =>
   new Promise(resolve => {
-    setTimeout(() => resolve(), 20000);
+    setTimeout(() => resolve(), 100);
   });
 
 let changeStream;
 async function run() {
   try {
     await client.connect();
-    const database = client.db('insert_db');
-    const movies = database.collection('haiku');
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
 
-    // open a Change Stream on the "haiku" collection
+    // open a Change Stream on the "movies" collection
     changeStream = movies.watch();
 
     // set up a listener when change events are emitted
     changeStream.on('change', next => {
       // process any change event
       console.log('received a change to the collection: \t', next);
+    });
+
+    await simulateAsyncPause();
+
+    await movies.insertOne({
+      test: 'sample movie document'
     });
 
     await simulateAsyncPause();
