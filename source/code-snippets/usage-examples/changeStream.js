c29d556ced40d56e56907e710b8831c21d80654e
Kailie Yuan <kailie.yuan@10gen.com>
2021-09-09 16:32:23 -0400

    updated to double quotes

--- source/code-snippets/usage-examples/changeStream.js
+++ source/code-snippets/usage-examples/changeStream.js
@@ -14,27 +14,27 @@ let changeStream;
 async function run() {
   try {
     await client.connect();
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
 
     // open a Change Stream on the "movies" collection
     changeStream = movies.watch();
 
     // set up a listener when change events are emitted
-    changeStream.on('change', next => {
+    changeStream.on("change", next => {
       // process any change event
-      console.log('received a change to the collection: \t', next);
+      console.log("received a change to the collection: \t", next);
     });
 
     await simulateAsyncPause();
 
     await movies.insertOne({
-      test: 'sample movie document'
+      test: "sample movie document"
     });
 
     await simulateAsyncPause();
 
-    await changeStream.close(() => console.log('closed the change stream'));
+    await changeStream.close(() => console.log("closed the change stream"));
   } finally {
     await client.close();
   }
