b7d0287c1a35928e34407128bce78d135b8fa9d9
Durran Jordan <durran@gmail.com>
2023-05-30 14:55:54 -0400

    docs: add suggestion and comment

--- source/code-snippets/authentication/oidc-callbacks.ts
+++ source/code-snippets/authentication/oidc-callbacks.ts
@@ -2,10 +2,12 @@ import { MongoClient } from 'mongodb';
 import { createMongoDBOIDCPlugin } from '@mongodb-js/oidc-plugin';
 
 // All config options are optional.
+// Please see https://github.com/mongodb-js/oidc-plugin/#example-usage for more information.
 const config = {
   openBrowser: {
     command: 'open -a "Firefox"',
   },
+  allowedFlows: ['auth-code']
 };
 
 const client = await MongoClient.connect(
