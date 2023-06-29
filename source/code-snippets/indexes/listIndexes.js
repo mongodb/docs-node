02d29556d51d62c372f6eb62a5b45b28fbc5c745
nickldp <135850974+nickldp@users.noreply.github.com>
2023-06-29 13:13:29 -0400

    DOCSP-30756-listIndexes-v5.5-and-earlier (#705)
    * DOCSP-30756-listIndexes-v5.5-and-earlier
    
    * DOCSP-30756-listIndexes-v5.5-and-earlier
    
    * final fix

---
+++ source/code-snippets/indexes/listIndexes.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the following with your MongoDB deployment's connection
+// string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
+
+const client = new MongoClient(uri);
+
+async function run(){
+    try {
+        const database = client.db("<database>");
+        const collection = database.collection("<collection>");
+        // start-listIndexes-example
+        const result = await collection.listIndexes().toArray();
+        console.log("Existing indexes:\n");
+        for(const doc in result){
+            console.log(doc);
+        }
+        // end-listIndexes-example
+    }
+    finally{
+        await client.close();
+    }
+}
+
+
