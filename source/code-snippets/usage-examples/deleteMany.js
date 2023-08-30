a53467becb08d81f5f24bd9d79ad89d8e87cbe0c
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-08-30 11:15:31 -0400

    DOCSP-32718: deletemany UE code comments

--- source/code-snippets/usage-examples/deleteMany.js
+++ source/code-snippets/usage-examples/deleteMany.js
@@ -1,21 +1,32 @@
+/* Delete multiple documents */
+
+// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "<connection string uri>";
 
+// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
 async function run() {
   try {
+    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
-    // Query for all movies with a title containing the string "Santa"
+
+    // Create a filter for movies with a title containing the string "Santa".
     const query = { title: { $regex: "Santa" } };
 
+    // Delete all documents that match the preceding query filter.
     const result = await movies.deleteMany(query);
+
+    // Print the number of deleted documents.
     console.log("Deleted " + result.deletedCount + " documents");
   } finally {
+    // Close the client after the operation completes.
     await client.close();
   }
 }
+// Run the program and handle any errors that occur during execution.
 run().catch(console.dir);
