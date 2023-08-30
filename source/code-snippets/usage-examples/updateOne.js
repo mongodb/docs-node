5828a69090daa0c58b0f7472709bc17eab5cd344
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-08-30 11:32:28 -0400

    DOCSP-32718: updateone UE code comments

--- source/code-snippets/usage-examples/updateOne.js
+++ source/code-snippets/usage-examples/updateOne.js
@@ -1,34 +1,45 @@
+/* Update a document */
+
+// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "<connection string uri>";
 
+// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
 async function run() {
   try {
+    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
-    // create a filter for a movie to update
+    // Create a filter for movies with the title "Random Harvest".
     const filter = { title: "Random Harvest" };
 
-    // this option instructs the method to create a document if no documents match the filter
+    // Set the upsert option to insert a document if no documents match the filter.
     const options = { upsert: true };
 
-    // create a document that sets the plot of the movie
+    // Specify the update to set a value for the plot field.
     const updateDoc = {
       $set: {
         plot: `A harvest of random numbers, such as: ${Math.random()}`
       },
     };
 
+    // Update the first document that matches the filter with the
+    // specified update.
     const result = await movies.updateOne(filter, updateDoc, options);
+    
+    // Print the number of matching documents and the number of modified documents.
     console.log(
       `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
     );
   } finally {
+    // Close the client after the operation completes.
     await client.close();
   }
 }
+// Run the program and handle any errors that occur during execution.
 run().catch(console.dir);
--- source/code-snippets/usage-examples/updateOne.ts
+++ source/code-snippets/usage-examples/updateOne.ts
@@ -1,10 +1,15 @@
+/* Update a document */
+
+// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "<connection string uri>";
 
+// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
+// Define the Movie interface to model documents in the collection.
 interface Movie {
   plot: string;
   title: string;
@@ -12,9 +17,12 @@ interface Movie {
 
 async function run() {
   try {
+    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection<Movie>("movies");
 
+    // Update a document that has the title "Random Harvest" to have a
+    // plot field with the specified value.
     const result = await movies.updateOne(
       { title: "Random Harvest" },
       {
@@ -22,13 +30,19 @@ async function run() {
           plot: `A harvest of random numbers, such as: ${Math.random()}`,
         },
       },
+      // Set the upsert option to insert a document if no documents
+      // match the filter.
       { upsert: true }
     );
+
+    // Print the number of matching documents and the number of modified documents.
     console.log(
       `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
     );
   } finally {
+    // Close the client after the operation completes.
     await client.close();
   }
 }
+// Run the program and handle any errors that occur during execution.
 run().catch(console.dir);
