2d3fd3eda7c10de0d20d0cac2faa856814f6dfd6
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-08-30 11:18:47 -0400

    add ts code

--- source/code-snippets/usage-examples/deleteMany.ts
+++ source/code-snippets/usage-examples/deleteMany.ts
@@ -1,22 +1,34 @@
+/* Delete multiple documents */
+
+// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "<connection string uri>";
 
+// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
+// Define the Movie interface to model the data.
 interface Movie {
   title: string;
 }
 
 async function run() {
   try {
+    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection<Movie>("movies");
+
+    // Delete all documents where the title contains the string "Santa".
     const result = await movies.deleteMany({ title: { $regex: "Santa" } });
+    
+    // Print the number of deleted documents.
     console.log("Deleted " + result.deletedCount + " documents");
   } finally {
+    // Close the client after the operation completes.
     await client.close();
   }
 }
+// Run the program and handle any errors that occur during execution.
 run().catch(console.dir);
