a9d4cd0f8af83f73df339add0e5d6893e9363934
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-08-30 11:53:33 -0400

    DOCSP-32718: listindexes code comments

--- source/code-snippets/indexes/listIndexes.js
+++ source/code-snippets/indexes/listIndexes.js
@@ -1,27 +1,35 @@
+/* List indexes */
+
+// Import the MongoClient type from the mongodb package.
 const { MongoClient } = require("mongodb");
 
-// Replace the following with your MongoDB deployment's connection
-// string.
+// Replace the placeholders with your credentials.
 const uri =
   "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
 
+// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
-import { MongoClient } from "mongodb";
+// Access a collection from a database.
 const database = client.db("<databaseName>");
 const collection = database.collection("<collectionName>");
 
 async function run() {
   try {
     // start listIndexes example
+    // List the indexes on the collection and output them as an array.
     const result = await collection.listIndexes().toArray();
+    
+    // Print the list of indexes.
     console.log("Existing indexes:\n");
     for(const doc in result){
         console.log(doc);
     }
     // end listIndexes example
   } finally {
+    // Close the client after the operation completes.
     await client.close();
   }
 }
+// Run the program and handle any errors that occur during execution.
 run().catch(console.dir);
