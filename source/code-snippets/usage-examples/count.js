00f4174ac092843421e5ffeec7e0e28fb47fba43
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-08-30 11:21:41 -0400

    DOCSP-32718: count UE code comments

--- source/code-snippets/usage-examples/count.js
+++ source/code-snippets/usage-examples/count.js
@@ -1,12 +1,18 @@
+/* Count documents in a collection*/
+
+// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "<connection string uri>";
 
+// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
 async function run() {
   try {
+
+    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
@@ -15,16 +21,17 @@ async function run() {
     const estimate = await movies.estimatedDocumentCount();
     console.log(`Estimated number of documents in the movies collection: ${estimate}`);
 
-    // Query for movies from Canada.
+    // Query for movies where the countries field includes "Canada".
     const query = { countries: "Canada" };
 
     // Find the number of documents that match the specified
-    // query, (i.e. with "Canada" as a value in the "countries" field)
-    // and print out the count.
+    // query and print out the count.
     const countCanada = await movies.countDocuments(query);
     console.log(`Number of movies from Canada: ${countCanada}`);
   } finally {
+    // Close the client after the operations complete.
     await client.close();
   }
 }
+// Run the program and handle any errors that occur during execution.
 run().catch(console.dir);
