48350bcfc0bbf2a2d9d2de544df9164f3bece956
Chris Cho <chris.cho@mongodb.com>
2023-08-30 11:47:38 -0400

    DOCSP-32718: add CodeWhisperer comments to SOCKS5 code snippet

--- source/code-snippets/connection/socks.js
+++ source/code-snippets/connection/socks.js
@@ -1,8 +1,10 @@
 import { MongoClient } from "mongodb";
 
 // start-socks
+// Replace the placeholder with your connection string
 const uri = "<connection string uri>";
 
+// Replace the placeholders with your SOCKS5 proxy server details
 const socksOptions = {
   proxyHost: "<host>",
   proxyPort: 1080,
@@ -13,6 +15,7 @@ const socksOptions = {
 const client = new MongoClient(uri, socksOptions);
 // end-socks
 
+// Retrieve the first document from the "myDB.myColl" namespace in MongoDB and print it to the console
 async function run() {
   try {
     const db = client.db("myDB");
