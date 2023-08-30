4215ba526cfbafec2623d6cc614ee885adc8dfb8
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-08-30 11:38:52 -0400

    DOCSP-32718: stableapi options code comments

--- source/code-snippets/stable-api/options.js
+++ source/code-snippets/stable-api/options.js
@@ -1,7 +1,16 @@
+/* Stable API - with options */
+
 // begin serverApiVersion
+// Import the MongoClient and ServerApiVersion types from the mongodb
+// package.
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
+// Replace the placeholders in the connection string uri with your credentials.
 const uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+
+// Create a client with options to specify Stable API Version 1, return
+// errors for commands outside of the API version, and raise exceptions
+// for deprecated commands.
 const client = new MongoClient(uri,
     {
         serverApi: {
