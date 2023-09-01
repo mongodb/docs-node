feb77bc71100d8acbcc7239ab4f88f2315d169a3
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-09-01 14:51:51 -0400

    fixes

--- source/code-snippets/usage-examples/updateOne.js
+++ source/code-snippets/usage-examples/updateOne.js
@@ -1,45 +1,42 @@
-/* Update a document */
+// Update a document
 
-// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
-// Replace the uri string with your MongoDB deployment's connection string.
+// Replace the uri string with your MongoDB deployment's connection string
 const uri = "<connection string uri>";
 
-// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
 async function run() {
   try {
-    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
-    // Create a filter for movies with the title "Random Harvest".
+    // Create a filter for movies with the title "Random Harvest"
     const filter = { title: "Random Harvest" };
 
-    // Set the upsert option to insert a document if no documents match the filter.
+    /* Set the upsert option to insert a document if no documents match
+    the filter */
     const options = { upsert: true };
 
-    // Specify the update to set a value for the plot field.
+    // Specify the update to set a value for the plot field
     const updateDoc = {
       $set: {
         plot: `A harvest of random numbers, such as: ${Math.random()}`
       },
     };
 
-    // Update the first document that matches the filter with the
-    // specified update.
+    // Update the first document that matches the filter
     const result = await movies.updateOne(filter, updateDoc, options);
     
-    // Print the number of matching documents and the number of modified documents.
+    // Print the number of matching and modified documents
     console.log(
       `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
     );
   } finally {
-    // Close the client after the operation completes.
+    // Close the connection after the operation completes
     await client.close();
   }
 }
-// Run the program and handle any errors that occur during execution.
+// Run the program and print any thrown errors
 run().catch(console.dir);
--- source/code-snippets/usage-examples/updateOne.ts
+++ source/code-snippets/usage-examples/updateOne.ts
@@ -1,15 +1,13 @@
-/* Update a document */
+// Update a document
 
-// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
-// Replace the uri string with your MongoDB deployment's connection string.
+// Replace the uri string with your MongoDB deployment's connection string
 const uri = "<connection string uri>";
 
-// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
-// Define the Movie interface to model documents in the collection.
+// Define the Movie interface
 interface Movie {
   plot: string;
   title: string;
@@ -17,12 +15,11 @@ interface Movie {
 
 async function run() {
   try {
-    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection<Movie>("movies");
 
-    // Update a document that has the title "Random Harvest" to have a
-    // plot field with the specified value.
+    /* Update a document that has the title "Random Harvest" to have a
+    plot field with the specified value */
     const result = await movies.updateOne(
       { title: "Random Harvest" },
       {
@@ -30,19 +27,19 @@ async function run() {
           plot: `A harvest of random numbers, such as: ${Math.random()}`,
         },
       },
-      // Set the upsert option to insert a document if no documents
-      // match the filter.
+      /* Set the upsert option to insert a document if no documents
+      match the filter */
       { upsert: true }
     );
 
-    // Print the number of matching documents and the number of modified documents.
+    // Print the number of matching and modified documents
     console.log(
       `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
     );
   } finally {
-    // Close the client after the operation completes.
+    // Close the connection after the operation completes
     await client.close();
   }
 }
-// Run the program and handle any errors that occur during execution.
+// Run the program and print any thrown errors
 run().catch(console.dir);
