f5e2c9860b3bc57ccb7a71c288e8ffacbc3e7064
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-09-01 14:58:29 -0400

    fixes

--- source/code-snippets/indexes/geo.js
+++ source/code-snippets/indexes/geo.js
@@ -1,32 +1,30 @@
-/* Create a geospatial index */
+// Create a geospatial index
 
-// Import the MongoClient type from the mongodb package.
 const { MongoClient } = require("mongodb");
 
-// Replace the placeholders with your MongoDB deployment's credentials.
+// Replace the placeholders with your MongoDB deployment's credentials
 const uri =
   "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
 
-// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
 async function run() {
   try {
     // begin-idx
-    // Access the movies collection from the sample_mflix database.
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
-    // Create a 2dsphere index on the "location.geo" field.
+    /* Create a 2dsphere index on the "location.geo" field in the
+    "movies" collection */
     const result = await movies.createIndex({ "location.geo": "2dsphere" });
     
-    // Print the result of the index creation.
+    // Print the result of the index creation
     console.log(`Index created: ${result}`);
     // end-idx
   } finally {
-    // Close the client after the operation completes.
+    // Close the connection after the operation completes
     await client.close();
   }
 }
-// Run the program and handle any errors that occur during execution.
+// Run the program and print thrown errors
 run().catch(console.dir);
