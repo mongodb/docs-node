fa9e623b6f9cf0656a542cd0ba6eb5cb0bf0d82f
Lindsey Moore <lindsey.moore@mongodb.com>
2025-03-20 21:44:29 -0400

    edits

--- source/includes/connect/connection-targets.js
+++ source/includes/connect/connection-targets.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 // Replace the placeholder with your Atlas connection string
 const uri = "<connection string>";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Creates a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri,  {
         serverApi: {
             version: ServerApiVersion.v1,
@@ -16,10 +16,10 @@ const client = new MongoClient(uri,  {
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
+    // Connects the client to the server (optional starting in v4.7)
     await client.connect();
 
-    // Send a ping to confirm a successful connection
+    // Sends a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
