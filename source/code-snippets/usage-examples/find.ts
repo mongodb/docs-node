83a9f445cfbcf9ee055187d58a606027f19cad95
Lindsey Moore <lindsey.moore@mongodb.com>
2025-04-22 17:05:17 -0400

    edit find ts example

--- source/code-snippets/usage-examples/find.ts
+++ source/code-snippets/usage-examples/find.ts
@@ -25,13 +25,10 @@ async function run() {
     const movies = database.collection<Movie>("movies");
 
     const query = { runtime: { $lt: 15 } };
-    const cursor = movies.find<Movie>(
-      query,
-      {
-        sort: { title: 1 },
-        projection: { _id: 0, title: 1, imdb: 1 },
-      }
-    );
+    const sort = { title: 1 };
+    const projection = { _id: 0, title: 1, imdb: 1 };
+
+    const cursor = movies.find<Movie>(query).sort(sort).project(projection);
 
     if ((await movies.countDocuments(query)) === 0) {
       console.warn("No documents found!");
