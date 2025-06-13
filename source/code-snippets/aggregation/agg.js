5275d65f38bd172a000568e57bdd9c4aed6d6e41
Melanie Ballard <melanie.ballard@mongodb.com>
2025-06-13 13:42:54 -0400

    DOCSP-50497 define framework with no concrete example, save for server pages

--- source/code-snippets/aggregation/agg.js
+++ source/code-snippets/aggregation/agg.js
@@ -10,24 +10,11 @@ async function run() {
     const db = client.db("aggregation");
     const coll = db.collection("restaurants");
 
-    // Create sample documents
-    const docs = [
-        { stars: 3, categories: ["Bakery", "Sandwiches"], name: "Rising Sun Bakery" },
-        { stars: 4, categories: ["Bakery", "Cafe", "Bar"], name: "Cafe au Late" },
-        { stars: 5, categories: ["Coffee", "Bakery"], name: "Liz's Coffee Bar" },
-        { stars: 3, categories: ["Steak", "Seafood"], name: "Oak Steakhouse" },
-        { stars: 4, categories: ["Bakery", "Dessert"], name: "Petit Cookie" },
-    ];
-
-    // Insert documents into the restaurants collection
-    const result = await coll.insertMany(docs);
-    // end data insertion
-
     // begin aggregation
     // Define an aggregation pipeline with a match stage and a group stage
     const pipeline = [
-        { $match: { categories: "Bakery" } },
-        { $group: { _id: "$stars", count: { $sum: 1 } } }
+        { $match: { cuisine: "Bakery" } },
+        { $group: { _id: "$borough", count: { $sum: 1 } } }
     ];
 
     // Execute the aggregation
